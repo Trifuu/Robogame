81da2571f764b8814fe26035660f503a337d89eb
Trifu Marius-Constantin <trifumarius01@gmail.com>
2018-01-20 16:54:33 +0200

    v3

--- src/main/java/de/metro/robocode/Yep.java
+++ src/main/java/de/metro/robocode/Yep.java
@@ -6,61 +6,73 @@ import java.awt.Color;
 public class Yep extends RateControlRobot {
 
     double deplasarea;
-    int ready=0;
-            
+    int ready = 0;
+    int first_time = 0;
+
     public void run() {
-        
+
+        init();
+
+        while (true) {
+
+            ready = 1;
+
+            ahead(calculate_and_move());
+
+            ready = 0;
+
+            turnRight(90);
+        }
+    }
+
+    public void init() {
+        //stabileste culorile
         setBodyColor(Color.orange);
         setGunColor(Color.red);
         setRadarColor(Color.yellow);
         setBulletColor(Color.black);
         setScanColor(Color.green);
 
-  
         deplasarea = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
-
         turnLeft(getHeading() % 90);
-        ahead(deplasarea);
-        
+        ahead(calculate_and_move());
+
         ready = 1;
-        
+
         turnGunRight(90);
         turnRight(90);
-
-        while (true) {
-            // Look before we turn when ahead() completes.
-            ready = 1;
-            // Move up the wall
-            ahead(deplasarea);
-            // Don't look now
-            ready = 0;
-            // Turn to the next wall
-            turnRight(90);
-        }
     }
 
-    /**
-     * onHitRobot: Move away a bit.
-     */
+    double calculate_and_move() {
+        if (getHeading() == 0) {
+            return (600 - getY()-1);
+        } else if (getHeading() == 90) {
+            return (600 - getX()-1);
+        } else if (getHeading() == 180) {
+            return (getY()-1);
+        } else if (getHeading() == 270) {
+            return (getX()-1);
+        }
+        return 800;
+    }
+
     public void onHitRobot(HitRobotEvent e) {
-        // If he's in front of us, set back up a bit.
+
         if (e.getBearing() > -90 && e.getBearing() < 90) {
             back(100);
-        } // else he's in back of us, so set ahead a bit.
-        else {
+        } else {
             ahead(100);
         }
     }
 
-    /**
-     * onScannedRobot: Fire!
-     */
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(3);
-        // Note that scan is called automatically when the robot is moving.
-        // By calling it manually here, we make sure we generate another scan event if there's a robot on the next
-        // wall, so that we do not start moving up it until it's gone.
-        if (ready==1) {
+        if (getOthers() > 10) {
+            fire(3);
+        } else {
+            fire(1);
+        }
+
+        if (ready == 1) {
             scan();
         }
     }
