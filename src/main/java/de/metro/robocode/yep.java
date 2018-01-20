a569bf020165fd4111c5d95544fd0d3c378de0fa
Trifu Marius-Constantin <trifumarius01@gmail.com>
2018-01-20 13:15:52 +0200

    varianta1

--- src/main/java/de/metro/robocode/yep.java
+++ src/main/java/de/metro/robocode/yep.java
@@ -1,13 +1,16 @@
 package main.java.de.metro.robocode;
 
 import robocode.*;
+import java.awt.Color;
+import java.util.*;
 
-public class TestBot extends Robot {
 
-    /**
-	 * run: Trifu's default behavior
+public class yep extends Robot {
+
+   	int i=0;	
+	/**
+	 * run: Robo2's default behavior
 	 */
-        @Override
 	public void run() {
 		// Initialization of the robot should be put here
 
@@ -19,18 +22,14 @@ public class TestBot extends Robot {
 		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			ahead(1200);
+			
 		}
 	}
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
-     * @param e
 	 */
-        @Override
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Replace the next line with any behavior you would like
 		fire(1);
@@ -38,9 +37,7 @@ public class TestBot extends Robot {
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
-     * @param e
 	 */
-        @Override
 	public void onHitByBullet(HitByBulletEvent e) {
 		// Replace the next line with any behavior you would like
 		back(10);
@@ -48,12 +45,11 @@ public class TestBot extends Robot {
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
-     * @param e
 	 */
-        @Override
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
 		back(20);
-	}
+		turnRight(90);
+	}		
 
 }
