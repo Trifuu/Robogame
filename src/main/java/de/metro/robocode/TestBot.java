6104a08be0705c13b3cdf7b6b879702ececf4362
Trifu Marius-Constantin <trifumarius01@gmail.com>
2018-01-20 11:02:30 +0200

    test

--- src/main/java/de/metro/robocode/TestBot.java
+++ src/main/java/de/metro/robocode/TestBot.java
@@ -1,29 +1,59 @@
-package de.metro.robocode;
+package main.java.de.metro.robocode;
 
 import robocode.*;
 
 public class TestBot extends Robot {
 
-    @Override
-    public void run() {
+    /**
+	 * run: Trifu's default behavior
+	 */
+        @Override
+	public void run() {
+		// Initialization of the robot should be put here
 
-        double radius = 100.0;
-        double angle = 90.0;
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
 
-        while (true) {
-            ahead(radius);
-            turnLeft(angle);
-            turnGunLeft(angle);
-            fireBullet(getEnergy());
-        }
-    }
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
 
-    public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
-    }
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
 
-    public void onHitByBullet(HitByBulletEvent e) {
-        turnLeft(90 - e.getBearing());
-    }
+	/**
+	 * onScannedRobot: What to do when you see another robot
+     * @param e
+	 */
+        @Override
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+     * @param e
+	 */
+        @Override
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+     * @param e
+	 */
+        @Override
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}
 
 }
